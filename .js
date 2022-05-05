7cf09646437e3cf768f48fbc85d4c63591a090ac
AmnD4 <104925681+AmanDaa4@users.noreply.github.com>
2022-05-06 04:19:20 +0800

    Update .js

--- .js
+++ .js
@@ -10,47 +10,29 @@
 // ==/UserScript==
 
 
-function addCss(cssCode) {
-    var styleElement = document.createElement("style");
-    if (styleElement.styleSheet) {
-      styleElement.styleSheet.cssText = cssCode;
-    } else {
-      styleElement.appendChild(document.createTextNode(cssCode));
-    }
-    document.getElementsByTagName("head")[0].appendChild(styleElement);
-  }
-  
-  /**
-   * @param {string} url
-   */
-  async function $import(url) {
-    let css = await fetch(url).then((x) => x.text());
-    addCss(css);
-  }
-  
-  /**
-   * @param {string} url
-   */
-  async function $require(url) {
-    let js = await fetch(url).then((x) => x.text());
-    Function(js)();
-  }
-  
-  let uBababot = {
-    cImport: $import,
-    jRequire: $require,
-  };
-  window["uBababot"] = uBababot;
-  
-  Function.prototype.clone = function() {
-      var that = this;
-      var temp = function temporary() { return that.apply(this, arguments); };
-      for(var key in this) {
-          if (this.hasOwnProperty(key)) {
-              temp[key] = this[key];
-          }
-      }
-      return temp;
-  };
+    (function() {
+    'use strict';
+    var $ = window.jQuery;
+    $(document).on('keyup', function(e) {
+        if (e.which == 56) {
+            client.mode('image');
+            client.lock = true
+            toastr.info('ImageMode')
+        }
+    });
+})();
+
+        (function() {
+    'use strict';
+    var $ = window.jQuery;
+    $(document).on('keyup', function(e) {
+        if (e.which == 57) {
+            client.mode('none');
+            client.lock = false
+            toastr.info('BrushMode')
+        }
+    });
+
+})();
   
 
